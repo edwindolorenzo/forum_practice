34f16dbcb803e3e817098a6a31bfd8d003822e6d
edwindolorenzo <edwindo_lorenzo@yahoo.com>
2019-09-12 16:13:24 +0700

    update admin users

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,4 +1,26 @@
 class Admins::UsersController < AdminsController
   def index
+    @q = User.ransack(params[:q])
+    @users = @q.result
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admins_users_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params[:user].permit(:name, :email, :password)
+  end
+
 end
